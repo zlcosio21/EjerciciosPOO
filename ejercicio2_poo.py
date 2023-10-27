99f3e37f4fabb7f5681061d7dc714a80145e372c
Sebastián <126442441+zlcosio21@users.noreply.github.com>
2023-10-27 14:04:27 -0500

    Ejercicio2 Mejorado

--- ejercicio2_poo.py
+++ ejercicio2_poo.py
@@ -1,31 +1,29 @@
-#Crear una clase llamada CuentaBancaria con las siguientes especificaciones:   a. La clase debe tener los atributos numero_de_cuenta , saldo , y propietario b. La clase debe tener un método para depositar dinero en la cuenta.          c. La clase debe tener un método para retirar dinero de la cuenta.
-
+# Crear una clase llamada CuentaBancaria con las siguientes especificaciones:
+# a. La clase debe tener los atributos numero_de_cuenta , saldo , y propietario
+# b. La clase debe tener un método para depositar dinero en la cuenta.         
+# c. La clase debe tener un método para retirar dinero de la cuenta.
 
 class CuentaBancaria:
   def __init__(self, numero_de_cuenta, saldo, propietario):
     self.numero_de_cuenta = numero_de_cuenta
     self.saldo = saldo
     self.propietario = propietario
-
-  def depositar(self, depositar_dinero):
-    if depositar_dinero > 0:
-      self.saldo += depositar_dinero
-      print(f"Se ha depositado ${depositar_dinero} su saldo es de ${self.saldo}.")
-    else:
-      print("El monto a depositar debe ser mayor a cero.")
-
-  def retirar(self, retirar_dinero):
-    if retirar_dinero <= self.saldo:
-      self.saldo -= retirar_dinero
-      print(f"Se ha retirado ${retirar_dinero} su saldo es de ${self.saldo}. \n")
-    else:
-      print("El monto a retirar es mayor a su saldo. \n")
-
-
-cuenta_usuario1 = CuentaBancaria (210606, 0, "Sebastián")
-cuenta_usuario1.depositar(2000)
-cuenta_usuario1.retirar(400)
-
-cuenta_usuario2 = CuentaBancaria (11092011, 0, "JohnCena")
-cuenta_usuario2.depositar(0)
-cuenta_usuario2.retirar(100)
+    
+  def depositar(self, dinero_depositado):
+    if dinero_depositado > 0:
+      self.saldo += dinero_depositado
+      return f"Se ha depositado ${dinero_depositado} a su cuenta"
+           
+    return "Debe ingresar un monto mayor a 0"
+    
+  def retirar(self, dinero_retirado):
+    if dinero_retirado <= self.saldo:
+      self.saldo -= dinero_retirado
+      return f"Se ha retirado un monto de {dinero_retirado} de su cuenta"
+            
+    return f"No cuenta con el suficiente saldo en su cuenta su saldo es de {self.saldo}"
+        
+propietario1 = CuentaBancaria("3105217565", 500, "Sebastian")
+print(propietario1.depositar(500))
+print(propietario1.retirar(800))
+print(propietario1.saldo)
