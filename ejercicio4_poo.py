b4082ba5d6cb1d7a355396acc0d164b944fed3a3
Sebastián <126442441+zlcosio21@users.noreply.github.com>
2023-10-28 22:44:27 -0700

    Ejercicio 4 Mejorado

--- ejercicio4_poo.py
+++ ejercicio4_poo.py
@@ -1,8 +1,7 @@
 # Moral de Perro S.A. desea crear un comercio electrónico, pero no cuentan con el software para lograrlo, ayúdelos creando: una clase Producto y una clase CarritoDeCompras , con las siguientes especificaciones: 
 # a. La clase Producto debe tener dos atributos: nombre , y precio                     
-# b. La clase Producto debe tener un método que permita modificar el nombre del producto.
-# c. La clase CarritoDeCompras debe tener un atributo llamado productos que será una lista de elementos en el carrito
-# d. La clase CarritoDeCompras debe tener los siguientes métodos: agregar_item(self, producto) - un método que permita agregar un producto al carrito si éste aún no está en la lista. eliminar_item(self, producto) - un método que permita eliminar un producto del carrito si éste está en la lista. Si el producto no está en la lista, el método debe imprimir un mensaje de error. obtener_items(self) - un método que devuelve la lista de productos en el carrito. obtener_total(self) - un método que calcula y devuelve el costo total de todos los productos en el carrito.     
+# b. La clase CarritoDeCompras debe tener un atributo llamado productos que será una lista de elementos en el carrito
+# c. La clase CarritoDeCompras debe tener los siguientes métodos: agregar_item(self, producto) - un método que permita agregar un producto al carrito si éste aún no está en la lista. eliminar_item(self, producto) - un método que permita eliminar un producto del carrito si éste está en la lista. Si el producto no está en la lista, el método debe imprimir un mensaje de error. obtener_items(self) - un método que devuelve la lista de productos en el carrito. obtener_total(self) - un método que calcula y devuelve el costo total de todos los productos en el carrito.     
 # NOTA: Los elementos en la lista del carrito de compras deben ser productos, es decir, objetos creados con la clase Producto.""""
 
 class Producto:
